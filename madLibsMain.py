1c688d51b52a9131f5e809a10aa73e427a256937
Javin <47981364+javin7@users.noreply.github.com>
2021-05-26 11:48:09 -0400

    Add files via upload

---
+++ madLibsMain.py
@@ -0,0 +1,8 @@
+from madLibsSets import holiday
+
+print("Welcome to the Mad libs python game!")
+print("Enter the word that matches the requirements.")
+
+setSelection = input("Select which Mad Libs set you would like to use?")
+
+holiday()
