b345168d84ecdc4b8443de7621b03ffce1183db6
Javin <47981364+javin7@users.noreply.github.com>
2021-05-26 14:41:14 -0400

    Selection options for mad libs sets

--- madLibsMain.py
+++ madLibsMain.py
@@ -1,8 +1,14 @@
 from madLibsSets import holiday
+from madLibsSets import theFunPark
 
 print("Welcome to the Mad libs python game!")
 print("Enter the word that matches the requirements.")
 
-setSelection = input("Select which Mad Libs set you would like to use?")
-
-holiday()
+while True:
+    setSelection = input("Select which Mad Libs set you would like to use?\n")
+    if setSelection.lower() == "holiday":
+        holiday()
+        break
+    elif setSelection.lower() == "the fun park":
+        theFunPark()
+        break
